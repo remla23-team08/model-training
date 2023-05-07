ee0f194b607d5067d9940246415d7c8f2fdcf1a3
JvanderSaag <jelmervandersaag@me.com>
2023-05-07 17:55:29 +0200

    Created placeholder for future preprocessing steps

---
+++ src/preprocessing.py
@@ -0,0 +1,10 @@
+from src.load_data import load_mfcc
+import pandas as pd
+import json 
+
+def preprocessing(X, y, processedjson_path):
+    # TBD data processing
+    X_proc = X
+    y_proc = y
+
+    return X_proc, y_proc
