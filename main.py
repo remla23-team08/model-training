4917405dc87c9c776c6deb60417a782637e9d469
JvanderSaag <jelmervandersaag@me.com>
2023-05-07 18:06:53 +0200

    Updated main.py to include all pipeline steps

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from IPython.display import clear_output
 import os.path
-from src.load_data import save_mfcc, load_mfcc
+from src.load_data import save_mfcc
 from src.visualisation import visualise
-from src.preprocessing import preprocessing
+from src.classification import main_classification
+
 # Define filepaths
 dataset_path = r"Data/genres_original"
 json_path = r"res/data.json"
 
-# Load data into json file 
-if os.path.isfile(json_path):
-    # If the file exists, load mfcc json
-    X, y = load_mfcc(json_path)
-else: # If not, create data.json with mfcc features
+# Create data.json containing mfcc features if it does not already exist
+if not os.path.isfile(json_path):
     save_mfcc(dataset_path,json_path,num_segments=10)
     clear_output()
-    X, y = load_mfcc(json_path)
 
 # Visualise .wav file of any song
-vis = False
+vis = False # Toggle
+songpath = r"Data/genres_original/blues/blues.0000"
 if vis:
-    songpath = r"Data/genres_original/blues/blues.0000"
     visualise(dataset_path, json_path, songpath)
 
-# Preprocessing
+# Define and train classifier
+model = main_classification(json_path, show_history=True)
+
+model.save(r"res/model.h5")
