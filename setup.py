143d837593ee21019160c954caccbff3250c8a76
Alexandru Moraru <47633771+alemoraru@users.noreply.github.com>
2023-06-05 21:37:32 +0200

    Configure poetry. (#8)

--- setup.py
+++ setup.py
@@ -2,29 +2,29 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     author="Team 08",
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="The model-training repository of Team 08 for the CS4295 course at the TU Delft.",
     license="MIT license",
     include_package_data=True,
-    keywords='model_training',
-    name='model_training',
-    packages=find_packages(include=['model_training', 'model_training.*']),
-    test_suite='tests',
-    url='https://github.com/remla23-team08/model-training',
-    version='0.2.0',
+    keywords="model_training",
+    name="model_training",
+    packages=find_packages(include=["model_training", "model_training.*"]),
+    test_suite="tests",
+    url="https://github.com/remla23-team08/model-training",
+    version="0.2.0",
     zip_safe=False,
 )
--- src/pipeline/load_data.py
+++ src/pipeline/load_data.py
@@ -8,15 +8,21 @@ import os
 import urllib.request
 import zipfile
 
-
 if __name__ == "__main__":
-       # Specify the relative path to data tsv
+    # Specify the relative path to data tsv
     root_path = os.path.dirname(os.path.abspath(__file__))
-    dataset_path = os.path.join(root_path, '..', '..', 'data', 'external', 'a1_RestaurantReviews_HistoricDump.tsv')
+    dataset_path = os.path.join(
+        root_path,
+        "..",
+        "..",
+        "data",
+        "external",
+        "a1_RestaurantReviews_HistoricDump.tsv",
+    )
 
     # Import the data from external source
     print("Importing external dataset..")
-    URL = r'https://drive.google.com/uc?export=download&id=1G7rLkSloPUzkK4zCzb9lLR0zSYygu8mK'
+    URL = r"https://drive.google.com/uc?export=download&id=1G7rLkSloPUzkK4zCzb9lLR0zSYygu8mK"
     zip_path, _ = urllib.request.urlretrieve(URL)
 
     # Define export path for dataset
